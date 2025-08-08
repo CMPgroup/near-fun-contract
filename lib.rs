e2d3b2f4920c9a453a44102fb654426e7b173586
CMPgroup <161823893+CMPgroup@users.noreply.github.com>
2025-08-08 09:38:44 +0200

    Update lib.rs

--- lib.rs
+++ lib.rs
@@ -1,57 +1,31 @@
-// We now import the NearToken type
-use near_sdk::{env, near, AccountId, NearToken, Promise};
+use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
+use near_sdk::{env, near_bindgen, require, AccountId};
 
-// We define our constant as a NearToken from the start
-const TOKEN_CREATION_FEE: NearToken = NearToken::from_yoctonear(100_000_000_000_000_000_000_000); // 0.5 NEAR
-
-#[near(contract_state)]
+#[near_bindgen]
+#[derive(BorshDeserialize, BorshSerialize)]
 pub struct Contract {
     pub owner_id: AccountId,
+    pub is_initialized: bool,
 }
 
+// We need this because AccountId does not have a default value.
 impl Default for Contract {
     fn default() -> Self {
-        env::panic_str("Contract must be initialized");
+        Self {
+            owner_id: "system".parse().unwrap(), // Placeholder
+            is_initialized: false,
+        }
     }
 }
 
-#[near]
+#[near_bindgen]
 impl Contract {
     #[init]
-    pub fn new(owner_id: AccountId) -> Self {
+    pub fn new() -> Self {
+        require!(!env::state_exists(), "The contract is already initialized");
         Self {
-            owner_id,
+            owner_id: env::predecessor_account_id(),
+            is_initialized: true,
         }
-    }
-
-    #[payable]
-    pub fn create_token(
-        &mut self,
-        token_name: String,
-        // We add underscores to the unused variables
-        _token_symbol: String,
-        _token_icon: String,
-        _token_decimals: u8,
-    ) {
-        let attached_deposit = env::attached_deposit();
-
-        // Now we are comparing two NearToken types, which is correct
-        assert!(
-            attached_deposit.ge(&TOKEN_CREATION_FEE),
-            "Attached deposit must be at least {} yoctoNEAR", TOKEN_CREATION_FEE.as_yoctonear()
-        );
-
-        // This comparison also works correctly now
-        if attached_deposit.gt(&TOKEN_CREATION_FEE) {
-            // And subtraction works too
-            let refund = attached_deposit.saturating_sub(TOKEN_CREATION_FEE);
-            Promise::new(env::predecessor_account_id()).transfer(refund);
-        }
-
-        env::log_str(&format!(
-            "Token '{}' created by {}.",
-            token_name,
-            env::predecessor_account_id()
-        ));
     }
 }
